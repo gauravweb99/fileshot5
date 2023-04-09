753f3b6ddffe8faf4ac203c4f08286ca734cbd6d
Gaurav Web99 <126997901+gauravweb99@users.noreply.github.com>
2023-04-09 07:03:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,58 +2,67 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5825503942:AAEKkAKE5BrBI4DIYRa_OsG3jqGovCSb7R4")
+TG_BOT_TOKEN = os.environ.get(
+  "TG_BOT_TOKEN", "5854093607:AAHZ-xtK3DHDXieT0luR7P0LViiOoyOf-ig")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "17850184"))
+APP_ID = int(os.environ.get("APP_ID", "11983148"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "a9607d2b34098b079b6365db3131fd6e")
+API_HASH = os.environ.get("API_HASH", "1168e573def0c74a7e6e68dae9313c68")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", " -1001590974706"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001730396391"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "2079962173"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1230839024"))
 
 #Port
-PORT = os.environ.get("PORT", None)
+PORT = os.environ.get("PORT", "8080")
 
-#Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://manju:1234@cluster0.s6qpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot6896666668")
+#Database
+DB_URI = os.environ.get(
+  "DATABASE_URL",
+  "mongodb+srv://RF999:RF999@cluster0.uaznbh4.mongodb.net/?retryWrites=true&w=majority"
+)
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot2")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001344341956"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "👋 Hello {first}\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get(
+  "START_MESSAGE",
+  "👋 Hello {first}\nI can store private files in Specified Channel and other users can access it from special link."
+)
 try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5863383503").split()):
-        ADMINS.append(int(x))
+  ADMINS = []
+  for x in (os.environ.get("ADMINS", "5863383503").split()):
+    ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+  raise Exception("Your Admins list does not contain valid integers.")
 
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "👋 Hello {first}\n<b>😓 You need to join in my Channel/Group to use me\n😋 Kindly Please join Channel</b>")
+#Force sub message
+FORCE_MSG = os.environ.get(
+  "FORCE_SUB_MESSAGE",
+  "👋 Hello {first}\n<b>😓 You need to join in my Channel/Group to use me\n😋 Kindly Please join Channel</b>"
+)
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT',
+                                         "False") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
-    DISABLE_CHANNEL_BUTTON = True
+  DISABLE_CHANNEL_BUTTON = True
 else:
-    DISABLE_CHANNEL_BUTTON = False
+  DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = ""
@@ -64,20 +73,15 @@ ADMINS.append(1250450587)
 LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
-    handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
-)
+  level=logging.INFO,
+  format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+  datefmt='%d-%b-%y %H:%M:%S',
+  handlers=[
+    RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+    logging.StreamHandler()
+  ])
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+  return logging.getLogger(name)
